57db4c5a03dfcfb998ea65676befc46c526c7d02
Paulo Gutemberg <pgutembergss@gmail.com>
2018-06-20 10:23:29 -0300

    Criando o metodo de alterar o cadastro

--- app/src/main/java/com/example/caririinovacao/minhaagenda/CadEventoActivity.java
+++ app/src/main/java/com/example/caririinovacao/minhaagenda/CadEventoActivity.java
@@ -1,16 +1,17 @@
 package com.example.caririinovacao.minhaagenda;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.*;
+
 
 public class CadEventoActivity extends AppCompatActivity {
 
-    String desc, tipo, hora, data, resultado;
+    String desc, tipo, hora, data, resultado, codigo;
     EditText etDesc, etTipo, etHora, etData;
+    Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +23,19 @@ public class CadEventoActivity extends AppCompatActivity {
          etHora = (EditText)findViewById(R.id.txt_hora);
          etData = (EditText)findViewById(R.id.txt_data);
 
-        ImageButton btn_salvar = (ImageButton)findViewById(R.id.btn_salvar);
+
+         if (getIntent().getStringExtra("situacao").equals("alterar")){
+            Controller c = new Controller(getBaseContext());
+            Toast.makeText(CadEventoActivity.this,"está entrando no if",Toast.LENGTH_LONG).show();
+            codigo = getIntent().getStringExtra("codigo");
+            cursor = c.buscaID(Integer.parseInt(codigo));
+            etDesc.setText(cursor.getString(cursor.getColumnIndexOrThrow("descr")));
+            etTipo.setText(cursor.getString(cursor.getColumnIndexOrThrow("tipo")));
+            etHora.setText(cursor.getString(cursor.getColumnIndexOrThrow("hora")));
+            etData.setText(cursor.getString(cursor.getColumnIndexOrThrow("data")));
+        }
+
+        Button btn_salvar = (Button)findViewById(R.id.btn_salvar);
         btn_salvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/caririinovacao/minhaagenda/ListarActivity.java
+++ app/src/main/java/com/example/caririinovacao/minhaagenda/ListarActivity.java
@@ -1,5 +1,7 @@
 package com.example.caririinovacao.minhaagenda;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,13 +10,17 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class ListarActivity extends AppCompatActivity {
 
     ListView lista;
     Cursor cursor;
+    String codigo;
+    AlertDialog alerta;
 
 
     @Override
@@ -22,15 +28,73 @@ public class ListarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar);
 
+
         Controller c = new Controller(getBaseContext());
         cursor = c.lista();
-        String[] campos = new String[]{"_id","desc","tipo","hora","data"};
+        String[] campos = new String[]{"_id","descr","tipo","hora","data"};
         int[] idViews = new int[]{R.id.id,R.id.desc,R.id.tipo, R.id.hora, R.id.data};
 
         SimpleCursorAdapter ad = new SimpleCursorAdapter(getBaseContext(),R.layout.layout_lista,cursor,campos,idViews,0);
 
         lista = (ListView)findViewById(R.id.listarEventos);
         lista.setAdapter(ad);
+
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                cursor.moveToPosition(i);
+                codigo = cursor.getString(cursor.getColumnIndexOrThrow("_id"));
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListarActivity.this);
+                builder.setMessage("O que deseja fazer?");
+                builder.setPositiveButton("Alterar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = new Intent(ListarActivity.this,CadEventoActivity.class);
+                        intent.putExtra("situacao","alterar");
+                        intent.putExtra("codigo",codigo);
+                        startActivity(intent);
+
+                    }
+                });
+
+                builder.setNegativeButton("Excluir", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ListarActivity.this);
+                        builder.setMessage("Deseja realmente excluir?");
+                        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Controller c = new Controller(getBaseContext());
+                                c.deletar(Integer.parseInt(codigo));
+                                //Controller c2 = new Controller(getBaseContext());
+                                cursor = c.lista();
+                                String[] campos = new String[]{"_id","descr","tipo","hora","data"};
+                                int[] idViews = new int[]{R.id.id,R.id.desc,R.id.tipo, R.id.hora, R.id.data};
+
+                                SimpleCursorAdapter ad = new SimpleCursorAdapter(getBaseContext(),R.layout.layout_lista,cursor,campos,idViews,0);
+
+                                lista = (ListView)findViewById(R.id.listarEventos);
+                                lista.setAdapter(ad);
+                                Toast.makeText(ListarActivity.this,"Deletado com sucesso",Toast.LENGTH_LONG).show();
+                            }
+                        });
+                        builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        });
+                        alerta = builder.create();
+                        alerta.show();
+
+                    }
+                });
+                alerta = builder.create();
+                alerta.show();
+            }
+        });
 
 
     }
